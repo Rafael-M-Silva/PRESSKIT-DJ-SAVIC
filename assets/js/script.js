ee8216297e4366a0f448054c31d4e3c11d612153
Rafael Mauricio <rafael_mauricio@hotmail.com>
2025-05-22 00:34:02 -0300

    feat: effect scroll

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+ScrollReveal().reveal(".paragraph", {
+  origin: "right", // direção do efeito
+  distance: "200px", // distância que o elemento se move
+  duration: 1000, // duração da animação em ms
+  delay: 1, // atraso antes de começar
+  reset: false, // se quiser que o efeito repita ao voltar
+});
+
+ScrollReveal().reveal(".midia-kit h2", {
+  origin: "left",
+  distance: "200px",
+  duration: 1000,
+  delay: 1,
+  reset: false,
+});
+
+ScrollReveal().reveal(".contato h2", {
+  origin: "left",
+  distance: "200px",
+  duration: 1000,
+  delay: 1,
+  reset: false,
+});
+ScrollReveal().reveal(".content", {
+  origin: "bottom",
+  distance: "200px",
+  duration: 1000,
+  delay: 1,
+  reset: false,
+});
+
+ScrollReveal().reveal(".midia-kit .videos video:nth-child(1)", {
+  origin: "left",
+  distance: "200px",
+  duration: 3000,
+  delay: 1,
+  reset: false,
+});
+ScrollReveal().reveal(".midia-kit .videos video:nth-child(2)", {
+  origin: "left",
+  distance: "200px",
+  duration: 2000,
+  delay: 1,
+  reset: false,
+});
+ScrollReveal().reveal(".midia-kit .videos video:nth-child(3)", {
+  origin: "left",
+  distance: "200px",
+  duration: 1000,
+  delay: 1,
+  reset: false,
+});
